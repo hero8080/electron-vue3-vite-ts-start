4da0898035389472cb853124723b5d05b5112e31
zhouzhangfeng <zzf808080>
2022-09-13 21:36:44 +0800

    feat：测试electron

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,48 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-
+import { fileURLToPath, URL } from "node:url";
+import path from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import electron, { onstart } from "vite-plugin-electron";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    electron({
+      main: {
+        entry: "electron/main/index.ts",
+        vite: {
+          build: {
+            // For Debug
+            sourcemap: true,
+            outDir: "dist/electron/main",
+          },
+          // Will start Electron via VSCode Debug
+          plugins: [process.env.VSCODE_DEBUG ? onstart() : null],
+        },
+      },
+      preload: {
+        input: {
+          // You can configure multiple preload here
+          index: path.join(__dirname, "electron/preload/index.ts"),
+        },
+        vite: {
+          build: {
+            // For Debug
+            sourcemap: "inline",
+            outDir: "dist/electron/preload",
+          },
+        },
+      },
+      // Enables use of Node.js API in the Renderer-process
+      // https://github.com/electron-vite/vite-plugin-electron/tree/main/packages/electron-renderer#electron-renderervite-serve
+      renderer: {},
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
